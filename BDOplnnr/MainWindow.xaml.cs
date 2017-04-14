3e241327fcc4b7ae00d8ad9327b2b8db2afe5fa4
Weede <vitali.jochim@gmx.de>
2017-04-14 09:26:26 +0200

    DummySettings

--- BDOplnnr/MainWindow.xaml.cs
+++ BDOplnnr/MainWindow.xaml.cs
@@ -12,17 +12,45 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace BDOplnnr
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
+    public class Node
+    {
+        [BsonId]
+        public string territory { get; set; }
+        [BsonElement("city")]
+        public string city { get; set; }
+    }
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            DbConnection();
+
+        }
+
+        private void DbConnection()
+        {
+            var DbServer = new MongoDB.Driver.MongoClient();
+            var mongo = new MongoDB.Driver.MongoClient();
+            var db = mongo.GetDatabase("test1");
+            try
+            {
+                db.CreateCollection("bdo_test1");
+            }
+            catch (MongoDB.Driver.MongoCommandException)
+            {
+                ;
+            }
+
+            var collection = db.GetCollection<Node>("bdo1");
+
+            var set = new Properties.Settings();
+            
         }
     }
 }
